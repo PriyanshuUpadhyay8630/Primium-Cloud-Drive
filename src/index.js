58ee72559d5d43fdf82c6319a28af487e70ccd9e
PriyanshuUpadhyay8630 <ushivani059@gmail.com>
2026-08-08 18:42:11 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const { google } = require('googleapis');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// MongoDB URI जो आपने कॉपी की थी
+const uri = "mongodb+srv:ushivani059_db_user:7VSUAjZn897I9DA9@cluster0.2ohgfto.mongodb.net/?appName=Primium-Cloud-Drive-1";
+const client = new MongoClient(uri);
+
+// Google Drive API की Credentials
+const CLIENT_ID = 'YOUR_CLIENT_ID_HERE';
+const CLIENT_SECRET = 'YOUR_CLIENT_SECRET_HERE';
+const REDIRECT_URI = 'YOUR_REDIRECT_URI_HERE';
+
+const oauth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+);
+
+app.get('/', (req, res) => {
+    res.send('Cloud Drive Backend is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
